921eb2a141adbd9b2e19215fff2428fdfd4d4402
WhiteClawConeseur <44951860+VelvetDuck@users.noreply.github.com>
2022-12-12 13:36:52 +0100

    Update AboutComponent.tsx

--- pages/component/AboutComponent.tsx
+++ pages/component/AboutComponent.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 export default function AboutComponent(){
     return (
         <Flex my='10rem' mx='auto' direction='column' bg='#101010' w={{
-            sm: '400px',
-            md: '500px',
-            lg: '800px'
+            base: '200px',
+            sm: '300px',
+            md: '400px',
+            lg: '700px'
         }} h='30rem' alignItems={{
             sm: 'center'
         }}>
@@ -15,11 +16,13 @@ export default function AboutComponent(){
                 marginTop: '30px'
             }}alt='alt' src='/userAvatar.jpg'></Image>
             <Text fontSize={{
+                base: '15px',
                 sm: '20px',
                 md: '30px'
             }} px='5' my='10' as='h2' color='white' textAlign='center'>Hello!</Text>
             <Text w='80%' color='white' my='auto'
             fontSize={{
+                base: '12px',
                 sm: '15px',
                 md: '17px',
                 lg: '20px'
